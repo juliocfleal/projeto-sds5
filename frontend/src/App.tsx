3095e3efe6298f10b1c8101f685bec5c76433648
Julio Leal <julio.leal@alunos.ifsuldeminas.edu.br>
2021-11-02 17:52:44 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
+import NavBar from "components/NavBar";
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Ola mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+
+      <div className="container">
+        <h1 className="text-primary">Ola mundo!</h1>
+        <DataTable />
+      </div>
+      <Footer />
+
+    </>
   );
 }
 
