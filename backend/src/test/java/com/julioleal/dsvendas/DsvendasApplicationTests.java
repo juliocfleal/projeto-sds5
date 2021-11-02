c73bd6e2567839b81e86991d5d64c911ffed1172
Julio Leal <julio.leal@alunos.ifsuldeminas.edu.br>
2021-11-02 00:42:04 -0300

    Project created

---
+++ backend/src/test/java/com/julioleal/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julioleal.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
