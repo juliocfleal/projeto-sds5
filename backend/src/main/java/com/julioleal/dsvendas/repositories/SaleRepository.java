0e3083e2a6c6470fe8253726bbce34268f5dcd57
Julio Leal <julio.leal@alunos.ifsuldeminas.edu.br>
2021-11-04 20:52:11 -0300

    Group by search

--- backend/src/main/java/com/julioleal/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/julioleal/dsvendas/repositories/SaleRepository.java
@@ -1,9 +1,23 @@
 package com.julioleal.dsvendas.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.julioleal.dsvendas.dto.SaleSuccessDTO;
+import com.julioleal.dsvendas.dto.SaleSumDTO;
 import com.julioleal.dsvendas.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long>{
+	
+	
+	@Query("SELECT NEW com.julioleal.dsvendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount)) "
+			+ " FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSumDTO> amountGroupedBySeller();
+	
+	@Query("SELECT NEW com.julioleal.dsvendas.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals)) "
+			+ " FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSuccessDTO> successGroupedBySeller();
 
 }
--- backend/src/main/java/com/julioleal/dsvendas/service/SaleService.java
+++ backend/src/main/java/com/julioleal/dsvendas/service/SaleService.java
@@ -1,5 +1,7 @@
 package com.julioleal.dsvendas.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.julioleal.dsvendas.dto.SaleDTO;
+import com.julioleal.dsvendas.dto.SaleSuccessDTO;
+import com.julioleal.dsvendas.dto.SaleSumDTO;
 import com.julioleal.dsvendas.entities.Sale;
 import com.julioleal.dsvendas.repositories.SaleRepository;
 import com.julioleal.dsvendas.repositories.SellerRepository;
@@ -26,5 +30,15 @@ public class SaleService {
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x ->new SaleDTO(x));
 		
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return repository.successGroupedBySeller();
 	}
 }
