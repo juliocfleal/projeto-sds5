e6b0b8d27ddd322b9fd6aa96917fde82767c669a
Julio Leal <julio.leal@alunos.ifsuldeminas.edu.br>
2021-11-06 16:52:35 -0300

    BarChart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,9 @@
 export type SaleSum = {
-  sellerName: string,
-  sum: number
+  sellerName: string;
+  sum: number;
+}
+export type SaleSuccess = {
+  sellerName: ScrollSetting;
+  visited: number;
+  deals: number;
 }
