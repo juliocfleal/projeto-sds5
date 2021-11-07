1e167e1d74b1b76b6102f64c8f7ba0e53fa0b798
Julio Leal <julio.leal@alunos.ifsuldeminas.edu.br>
2021-11-07 15:13:03 -0300

    Env fix yarn

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -20,7 +20,7 @@ const DataTable = () => {
     axios.get(`${BASE_URL}/sales?page=${activePage}&size=20&sort=date,desc`)
       .then(response => {
         setPage(response.data);
-        console.log(page)
+        //      console.log(page)
       });
   }, [activePage]);
 
